9b9f2b4de7b629c53ab830546eacb7eb08a70f86
鐘鼎皓 <jason_chung@zhongdinghaode-iMac.local>
2017-10-29 14:21:16 +0800

    Initial Commit

---
+++ MyGuessNum/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MyGuessNum
+//
+//  Created by 鐘鼎皓 on 2017/10/29.
+//  Copyright © 2017年 ChungTingHao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
